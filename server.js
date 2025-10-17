10fecacced65ee9bc328289916cee0376e2de05f
truogn <votruong1511@gmail.com>
2025-10-18 04:02:33 +0700

    FEAT: use in-memory database for demo deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const JWT_SECRET = 'your_jwt_secret_key_change_this_in_production';
 
 app.use(bodyParser.json());
